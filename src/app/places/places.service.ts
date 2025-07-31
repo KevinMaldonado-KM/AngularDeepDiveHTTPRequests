20af168f562dde29db8e6fe614397a9f5290e58b
Kévin MALDONADO <kevin.maldonado@etu.univ-amu.fr>
2025-07-31 23:51:50 +0200

    Implémentation d'une pop up d'erreur - SECTION 12 COURS 233 DONE

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,11 +3,13 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private errorService = inject(ErrorService);
   private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
@@ -40,7 +42,8 @@ export class PlacesService {
       placeId: place.id,
     }).pipe(
       catchError(error => {
-        this.userPlaces.set(prevPlaces)
+        this.userPlaces.set(prevPlaces);
+        this.errorService.showError('Failed to store selected place.')
         return throwError(() => new Error('Failed to store selected place.'))
       })
     );
